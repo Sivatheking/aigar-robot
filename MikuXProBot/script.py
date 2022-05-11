2bba2aafdaf0d69bf6857941a50be448fbc1c602
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-05-11 20:31:00 +0530

    Update script.py

--- MikuXProBot/script.py
+++ MikuXProBot/script.py
@@ -1,44 +1,38 @@
 PM_START_TEXT = """────「 [{}]({}) 」────\n*Hello {}!*\n*I'm An Anime Themed Advance Group Management Bot*\n\n❍ *Uptime:* `{}`\n❍ `{}` *Users, Across* `{}` *Chats*\n\n*Click The Help Button To Get List Of My Commands.*\Thanks To @Shukurenaiprobot For Repo"""
 
 MIKU_IMG = (
-      "https://telegra.ph/file/4f17fdcc9470cc62b8c09.jpg",
-      "https://telegra.ph/file/e34e99801eb78ed84cb93.jpg",
-      "https://telegra.ph/file/917359f33c042a0425a00.jpg",
-      "https://telegra.ph/file/ca91b17ebf942ca00d9a2.jpg",
-      "https://telegra.ph/file/686b843ef19f3deafec37.jpg",
-      "https://telegra.ph/file/04d0f3e168ecd407c11ee.jpg",
-      "https://telegra.ph/file/6d3f5d3c434282a42a9a9.jpg",
-      "https://telegra.ph/file/d1572c4fcace1ae1bc85a.jpg",
-      "https://telegra.ph/file/f74dde459c5079c80d4bf.jpg",
-      "https://telegra.ph/file/010f3e703df0501305498.jpg",
-      "https://telegra.ph/file/7a10635105006681efb13.jpg",
-      "https://telegra.ph/file/6f8529c890740533f942a.jpg",
-      "https://telegra.ph/file/981c0edfa6fc49248c734.jpg",
-      "https://telegra.ph/file/1c24b51d7d1830ee5b626.jpg",
-      "https://telegra.ph/file/6513527f063c4f00a6806.jpg",
+      "https://telegra.ph//file/117e4b3d875c01574541b.jpg"
+"https://telegra.ph//file/4e8d45d004ad4a4c97a25.jpg"
+"https://telegra.ph//file/bd72631c88242d8c83985.jpg"
+"https://telegra.ph//file/3b9e223da0f677c76efd7.jpg"
+"https://telegra.ph//file/af344b7bec833979c8f09.jpg"
+"https://telegra.ph//file/991501048665b6f12c66a.jpg"
+"https://telegra.ph//file/6da4cc8f469b96bf6898d.jpg"
+"https://telegra.ph//file/2ec9914f2704a0f540c0e.jpg"
+"https://telegra.ph//file/001aff36a89f9d5fd447a.jpg"
+"https://telegra.ph//file/b8e89ebb8a93b85d677ae.jpg"
+"https://telegra.ph//file/f7b2cd754e2c19cfe8ac2.jpg"
+"https://telegra.ph//file/c0e01e630029bef998ffa.jpg"
 )
 
 TEXXT = "<b>Heya</b> {} <b>Do you Like me?</b>"
 
 MIKU_N_IMG = (
-      "https://telegra.ph/file/4f17fdcc9470cc62b8c09.jpg",
-      "https://telegra.ph/file/e34e99801eb78ed84cb93.jpg",
-      "https://telegra.ph/file/917359f33c042a0425a00.jpg",
-      "https://telegra.ph/file/ca91b17ebf942ca00d9a2.jpg",
-      "https://telegra.ph/file/686b843ef19f3deafec37.jpg",
-      "https://telegra.ph/file/04d0f3e168ecd407c11ee.jpg",
-      "https://telegra.ph/file/6d3f5d3c434282a42a9a9.jpg",
-      "https://telegra.ph/file/d1572c4fcace1ae1bc85a.jpg",
-      "https://telegra.ph/file/f74dde459c5079c80d4bf.jpg",
-      "https://telegra.ph/file/010f3e703df0501305498.jpg",
-      "https://telegra.ph/file/7a10635105006681efb13.jpg",
-      "https://telegra.ph/file/6f8529c890740533f942a.jpg",
-      "https://telegra.ph/file/981c0edfa6fc49248c734.jpg",
-      "https://telegra.ph/file/1c24b51d7d1830ee5b626.jpg",
-      "https://telegra.ph/file/6513527f063c4f00a6806.jpg"
+      "https://telegra.ph//file/117e4b3d875c01574541b.jpg"
+"https://telegra.ph//file/4e8d45d004ad4a4c97a25.jpg"
+"https://telegra.ph//file/bd72631c88242d8c83985.jpg"
+"https://telegra.ph//file/3b9e223da0f677c76efd7.jpg"
+"https://telegra.ph//file/af344b7bec833979c8f09.jpg"
+"https://telegra.ph//file/991501048665b6f12c66a.jpg"
+"https://telegra.ph//file/6da4cc8f469b96bf6898d.jpg"
+"https://telegra.ph//file/2ec9914f2704a0f540c0e.jpg"
+"https://telegra.ph//file/001aff36a89f9d5fd447a.jpg"
+"https://telegra.ph//file/b8e89ebb8a93b85d677ae.jpg"
+"https://telegra.ph//file/f7b2cd754e2c19cfe8ac2.jpg"
+"https://telegra.ph//file/c0e01e630029bef998ffa.jpg"
 
 )
 
-PM_PHOTO = "https://telegra.ph/file/6513527f063c4f00a6806.jpg"
+PM_PHOTO = "https://telegra.ph//file/4e8d45d004ad4a4c97a25.jpg"
 
-MIKU_DISPACHER_PIC = "https://telegra.ph/file/981c0edfa6fc49248c734.jpg"
+MIKU_DISPACHER_PIC = "https://telegra.ph//file/4e8d45d004ad4a4c97a25.jpg"
