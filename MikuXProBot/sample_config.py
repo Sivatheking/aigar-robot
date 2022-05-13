7883d30d81f15b0190796cce7b70da21e7227d03
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-05-13 15:23:33 +0530

    Update sample_config.py

--- MikuXProBot/sample_config.py
+++ MikuXProBot/sample_config.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open('{}/MikuXProBot/{}'.format(os.getcwd(), config),
+    with open('{}/aigarrobot/{}'.format(os.getcwd(), config),
               'r') as json_file:
         return json.load(json_file)[key]
 
@@ -14,12 +14,12 @@ class Config(object):
     # REQUIRED
     #Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "awoo"  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "awoo"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 769830161 # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "awoo"
-    SUPPORT_CHAT = 'mikussupport'  #Your own group for support, do not add the @
+    API_ID = "{API_ID}"  # integer value, dont use ""
+    API_HASH = "{API_HASH}"
+    TOKEN = "{TOKEN"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = {OWNER_ID} # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "{OWNER_USERNAME}"
+    SUPPORT_CHAT = '{SUPPORT_CHAT}'  #Your own group for support, do not add the @
     JOIN_LOGGER = -123456789  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -123446789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
