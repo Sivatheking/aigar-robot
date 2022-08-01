1882c4b0f2b4b1d3306075057e0d5e17eeaa69a7
Sivatheking <105096041+Sivatheking@users.noreply.github.com>
2022-08-01 08:47:19 +0530

    Update sample_config.py

--- MikuXProBot/sample_config.py
+++ MikuXProBot/sample_config.py
@@ -3,7 +3,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open('{}/aigarrobot/{}'.format(os.getcwd(), config),
+    with open('{}/MikuXProBot.config/{}'.format(os.getcwd(), config),
               'r') as json_file:
         return json.load(json_file)[key]
 
@@ -18,7 +18,7 @@ class Config(object):
     API_HASH = "awoo"
     TOKEN = "awoo"  #This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = "5077267318" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sivatheking_2"
+    OWNER_USERNAME = "Sivatheking_1"
     SUPPORT_CHAT = 'shukurenai_chat'  #Your own group for support, do not add the @
     JOIN_LOGGER = -123456789  #Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = -123446789  #Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
